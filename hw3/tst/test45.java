1d4ae60e2a85197f9f30c723cb5106d3d37f2e0e
Ladinu Chandrasinghe <ladinu@gmail.com>
2014-02-01 18:13:30 -0800

    cluttered workspace

--- hw3/tst/test45.java
+++
@@ -1,9 +0,0 @@
-// test peephole optimizations
-// (should print 7)
-class Test {
-  public static void main(String[] x) {
-    int i = 2 + 3;
-    int j = 2 * 1;
-    System.out.println(i + j);
-  }
-}
