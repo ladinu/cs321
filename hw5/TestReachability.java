658cdf847507eb7b8e8c9ae1c5f1e66cdbe092d7
Ladinu Chandrasinghe <ladinu@gmail.com>
2014-03-05 17:19:53 -0800

    Added

---
+++ hw5/TestReachability.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import ast0.*;
+
+class TestReachability {
+  public static void main(String [] args) {
+    try {
+      if (args.length == 1) {
+	FileInputStream stream = new FileInputStream(args[0]);
+	Ast.Program p = new AstParser(stream).Program();
+	stream.close();
+	p.setReachability();
+	System.out.print(p);
+      } else {
+	System.out.println("Need an AST file name as command-line argument.");
+      }
+    } catch (TokenMgrError e) {
+      System.err.println(e.toString());
+    } catch (ParseException e) {
+      System.err.println(e.toString());
+    } catch (IOException e) {
+      System.err.println(e.toString());
+    }     
+  }
+}
