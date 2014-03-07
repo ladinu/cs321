40c46d51bebd5be4744ba1e99a33978ff6a89c91
Ladinu Chandrasinghe <ladinu@gmail.com>
2014-03-06 17:19:36 -0800

    test

--- hw5/test.sh
+++ hw5/test.sh
@@ -1,15 +1,22 @@
+#!/bin/sh
+function runTests() {
+   for testFile in `find $1 -name "*.ast"`; do
+      printf "Running $testFile: "
+      expected=${testFile%.*}.ref
+      java TestReachability $testFile > $1/pref.ref
+      result=`diff $expected $1/pref.ref`
+      if [ "$result" != "" ]; then
+         echo "FAIL"
+         diff $expected $1/pref.ref
+      else
+         echo ""
+      fi
+   done
+}
 
-for testFile in `find tests -name "*.ast"`; do
-   printf "Running $testFile: "
-   expected=${testFile%.*}.ref
-   java TestReachability $testFile > tests/pref.ref
-   result=`diff $expected tests/pref.ref`
-   if [ "$result" != "" ]; then
-      echo "FAIL"
-      echo $result
-   else
-      echo ""
-   fi
-done
+function clean() {
+   rm -rf $1/pref.ref
+}
 
-rm -rf tests/pref.ref
+runTests $1
+clean $1
